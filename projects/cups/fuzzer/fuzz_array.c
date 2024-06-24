96bc1617150001af0403ad6c664737d24e1fa1f6
TTFISH <jiongchiyu@gmail.com>
2024-06-24 09:52:40 +0800

    fix preliminary errors find in fuzzers
    Signed-off-by: TTFISH <jiongchiyu@gmail.com>

--- projects/cups/fuzzer/fuzz_array.c
+++ projects/cups/fuzzer/fuzz_array.c
@@ -15,7 +15,6 @@
 #include "dir.h"
 #include "test-internal.h"
 
-
 //
 // Local functions...
 //
@@ -30,34 +29,30 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 		*dup_array;		// Duplicate array
   int		status;			// Exit status
   char		*text;			// Text from array
-//   char		word[256];		// Word from file
-//   double	start,			// Start time
-		// end;			// End time
-//   cups_dir_t	*dir;			// Current directory
-//   cups_dentry_t	*dent;			// Directory entry
   char		*saved[32];		// Saved entries
-  // void		*data;			// User data for arrays
-
 
   // No errors so far...
   status = 0;
 
-  if (Size < 2) {
+  if (Size < 4) {
     return 0; 
   }
 
   size_t halfSize = Size / 2;
-  void *firstStr = malloc(halfSize + 1);
+  char *firstStr = (char *)malloc(halfSize + 1);
     if (!firstStr) {
         return 0;
     }
   memcpy(firstStr, Data, halfSize);
-  void *secondStr = malloc(Size - halfSize + 1);
+  firstStr[halfSize] = '\0';
+
+  char *secondStr = (char *)malloc(Size - halfSize + 1);
   if (!secondStr) {
     free(firstStr);
     return 0;
   }
   memcpy(secondStr, Data + halfSize, Size - halfSize);
+  secondStr[halfSize] = '\0';
 
   // cupsArrayNew()
   array = cupsArrayNew3((cups_array_func_t)_cupsArrayStrcmp, (void *)firstStr, NULL, 0, (cups_acopy_cb_t)_cupsArrayStrdup, (cups_afree_cb_t)_cupsArrayFree);
@@ -83,11 +78,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   }
 
   // cupsArrayGetCount()
-  if (cupsArrayGetCount(array) != halfSize)
-  {
-    printf("String Size Error");
-    abort();
-  }
+  cupsArrayGetCount(array);
 
   // cupsArrayGetFirst()
   text = (char *)cupsArrayGetFirst(array);
@@ -148,63 +139,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   }
 
   // Now load this source file and grab all of the unique words...
-
-//   start = get_seconds();
-
-//   if ((dir = cupsDirOpen(".")) == NULL)
-//   {
-//     testEndMessage(false, "cupsDirOpen failed");
-//     status ++;
-//   }
-//   else
-//   {
-//     bool load_status = true;		// Load status
-
-//     while ((dent = cupsDirRead(dir)) != NULL)
-//     {
-//       i = (int)strlen(dent->filename) - 2;
-
-//       if (i > 0 && dent->filename[i] == '.' && (dent->filename[i + 1] == 'c' || dent->filename[i + 1] == 'h'))
-//       {
-// 	if (!load_words(dent->filename, array))
-// 	{
-// 	  load_status = false;
-// 	  break;
-// 	}
-//       }
-//     }
-
-//     cupsDirClose(dir);
-
-//     if (load_status)
-//     {
-//       end = get_seconds();
-
-//       for (text = (char *)cupsArrayGetFirst(array); text;)
-//       {
-//         // Copy this word to the word buffer (safe because we strdup'd from
-// 	// the same buffer in the first place... :)
-// 	cupsCopyString(word, text, sizeof(word));
-
-//         // Grab the next word and compare...
-// 	if ((text = (char *)cupsArrayGetNext(array)) == NULL)
-// 	  break;
-
-// 	if (strcmp(word, text) >= 0)
-// 	  break;
-//       }
-
-//       if (text)
-//       {
-// 	testEndMessage(false, "\"%s\" >= \"%s\"", word, text);
-// 	status ++;
-//       }
-//       else
-//       {
-// 	testEndMessage(true, "%d words in %.3f seconds - %.0f words/sec", cupsArrayGetCount(array), end - start, cupsArrayGetCount(array) / (end - start));
-//       }
-//     }
-//   }
+  // TODO: do the file readings fuzzing
 
   // Test save/restore...
   for (i = 0, text = (char *)cupsArrayGetFirst(array); i < 32; i ++, text = (char *)cupsArrayGetNext(array))
@@ -233,63 +168,3 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
   return (status);
 }
-
-
-//
-// 'get_seconds()' - Get the current time in seconds...
-//
-
-// #ifdef _WIN32
-// #  include <windows.h>
-
-
-// static double
-// get_seconds(void)
-// {
-// }
-// #else
-// #  include <sys/time.h>
-
-
-// static double
-// get_seconds(void)
-// {
-//   struct timeval	curtime;	// Current time
-
-
-//   gettimeofday(&curtime, NULL);
-//   return (curtime.tv_sec + 0.000001 * curtime.tv_usec);
-// }
-// #endif // _WIN32
-
-
-//
-// 'load_words()' - Load words from a file.
-//
-
-// static int				// O - 1 on success, 0 on failure
-// load_words(const char   *filename,	// I - File to load
-//            cups_array_t *array)		// I - Array to add to
-// {
-//   FILE		*fp;			// Test file
-//   char		word[256];		// Word from file
-
-
-//   testProgress();
-
-//   if ((fp = fopen(filename, "r")) == NULL)
-//   {
-//     testEndMessage(false, "%s: %s", filename, strerror(errno));
-//     return (0);
-//   }
-
-//   while (fscanf(fp, "%255s", word) == 1)
-//   {
-//     if (!cupsArrayFind(array, word))
-//       cupsArrayAdd(array, word);
-//   }
-
-//   fclose(fp);
-
-//   return (1);
-// }
