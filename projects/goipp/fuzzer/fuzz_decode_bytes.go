3f45566e7460acf21f3d8367da2861a6a61fa504
mdimado <mdimad005@gmail.com>
2025-05-28 14:46:17 +0530

    init integration for goipp

---
+++ projects/goipp/fuzzer/fuzz_decode_bytes.go
@@ -0,0 +1,20 @@
+/*
+ * Fuzz target for goipp's `DecodeBytes` function.
+ */
+
+
+package fuzzer
+
+import (
+	"testing"
+	"github.com/OpenPrinting/goipp"
+)
+
+func FuzzDecodeBytes(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		var m goipp.Message
+		if err := m.DecodeBytes(data); err != nil {
+			t.Skip()
+		}
+	})
+}
