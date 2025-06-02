ec79e889899cf8f30332657f3cea8277bf0798d9
mdimado <mdimad005@gmail.com>
2025-06-02 22:29:40 +0530

    temporarily remove the decode bytes fuzz harness

--- projects/goipp/oss_fuzz_build.sh
+++ projects/goipp/oss_fuzz_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
 mkdir -p $SRC/goipp/fuzzer
-cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_decode_bytes.go $SRC/goipp/fuzzer/
+# cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_decode_bytes.go $SRC/goipp/fuzzer/
 cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_decode_bytes_ex.go $SRC/goipp/fuzzer/
 cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_round_trip.go $SRC/goipp/fuzzer/
 cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_collections.go $SRC/goipp/fuzzer/
@@ -11,7 +11,7 @@ cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_tag_extension.go $SRC/goipp/fuzzer/
 mkdir -p $WORK/good_and_bad_ipp_messages_seed_corpus
 cp $SRC/fuzzing/projects/goipp/seeds/good_and_bad_ipp_messages_seed_corpus/* $WORK/good_and_bad_ipp_messages_seed_corpus/
 cd $WORK
-zip -r $OUT/fuzz_decode_bytes_seed_corpus.zip good_and_bad_ipp_messages_seed_corpus/
+# zip -r $OUT/fuzz_decode_bytes_seed_corpus.zip good_and_bad_ipp_messages_seed_corpus/
 zip -r $OUT/fuzz_decode_bytes_ex_seed_corpus.zip good_and_bad_ipp_messages_seed_corpus/
 zip -r $OUT/fuzz_collections_seed_corpus.zip good_and_bad_ipp_messages_seed_corpus/
 
@@ -31,7 +31,7 @@ go mod tidy
 go install github.com/AdamKorcz/go-118-fuzz-build@latest
 go get github.com/AdamKorcz/go-118-fuzz-build/testing
 
-compile_native_go_fuzzer ./fuzzer FuzzDecodeBytes fuzz_decode_bytes
+# compile_native_go_fuzzer ./fuzzer FuzzDecodeBytes fuzz_decode_bytes
 compile_native_go_fuzzer ./fuzzer FuzzDecodeBytesEx fuzz_decode_bytes_ex
 compile_native_go_fuzzer ./fuzzer FuzzRoundTrip fuzz_round_trip
 compile_native_go_fuzzer ./fuzzer FuzzCollections fuzz_collections
