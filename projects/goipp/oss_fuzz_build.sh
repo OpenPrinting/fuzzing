9581fe89c05e97d5e482ae407ff4219920d9dced
mdimado <mdimad005@gmail.com>
2025-06-02 11:26:46 +0530

    removed the fuzz harness FuzzAttributeUnpacking

--- projects/goipp/oss_fuzz_build.sh
+++ projects/goipp/oss_fuzz_build.sh
@@ -6,7 +6,6 @@ cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_decode_bytes_ex.go $SRC/goipp/fuzzer/
 cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_round_trip.go $SRC/goipp/fuzzer/
 cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_collections.go $SRC/goipp/fuzzer/
 cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_tag_extension.go $SRC/goipp/fuzzer/
-cp $SRC/fuzzing/projects/goipp/fuzzer/fuzz_attribute_unpacking.go $SRC/goipp/fuzzer/  # new fuzz target
 
 # Corpus for fuzzers that accept good AND bad messages
 mkdir -p $WORK/good_and_bad_ipp_messages_seed_corpus
@@ -20,11 +19,6 @@ zip -r $OUT/fuzz_collections_seed_corpus.zip good_and_bad_ipp_messages_seed_corp
 mkdir -p $WORK/good_ipp_messages_seed_corpus
 cp $SRC/fuzzing/projects/goipp/seeds/good_ipp_messages_seed_corpus/* $WORK/good_ipp_messages_seed_corpus/
 zip -r $OUT/fuzz_round_trip_seed_corpus.zip good_ipp_messages_seed_corpus/
-
-# Corpus for tag extension 
-mkdir -p $WORK/extension_tags_seed_corpus
-cp $SRC/fuzzing/projects/goipp/seeds/extension_tags_seed_corpus/* $WORK/extension_tags_seed_corpus/
-zip -r $OUT/fuzz_tag_extension_seed_corpus.zip extension_tags_seed_corpus/
 
 # Corpus for attribute unpacking seeds
 mkdir -p $WORK/attr_unpack_seed_corpus
@@ -42,4 +36,3 @@ compile_native_go_fuzzer ./fuzzer FuzzDecodeBytesEx fuzz_decode_bytes_ex
 compile_native_go_fuzzer ./fuzzer FuzzRoundTrip fuzz_round_trip
 compile_native_go_fuzzer ./fuzzer FuzzCollections fuzz_collections
 compile_native_go_fuzzer ./fuzzer FuzzTagExtension fuzz_tag_extension
-compile_native_go_fuzzer ./fuzzer FuzzAttributeUnpacking fuzz_attribute_unpacking
