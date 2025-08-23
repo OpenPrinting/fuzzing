0dda20d78f8dc45405a3f7d1169a26b4274869dc
mdimado <mdimad005@gmail.com>
2025-08-23 14:01:16 +0530

    add seeds for utf8 fuzzer

--- projects/pycups/oss_fuzz_build.sh
+++ projects/pycups/oss_fuzz_build.sh
@@ -4,7 +4,14 @@
 cd $SRC/pycups
 pip3 install .
 
-# compile all fuzzers
+# compile all fuzzers with seeds
 for fuzzer in $(find $SRC/fuzzing/projects/pycups/fuzzer -name 'fuzz_*.py'); do
-  compile_python_fuzzer $fuzzer --hidden-import=cffi
+  fuzzer_name=$(basename "$fuzzer" .py)
+  
+  seed_dir="$SRC/fuzzing/projects/pycups/seeds/$fuzzer_name"
+  if [ -d "$seed_dir" ]; then
+    cd "$seed_dir" && zip -r "$OUT/${fuzzer_name}_seed_corpus.zip" .
+  fi
+  
+  compile_python_fuzzer "$fuzzer" --hidden-import=cups
 done
