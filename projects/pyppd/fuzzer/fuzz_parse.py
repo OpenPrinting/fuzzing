c41d460c735bad38c7da9665f4ec63e42c7a11b8
mdimado <mdimad005@gmail.com>
2025-08-26 11:41:06 +0530

    initialize fuzzers for pyppd

---
+++ projects/pyppd/fuzzer/fuzz_parse.py
@@ -0,0 +1,18 @@
+import atheris
+import sys
+import pyppd.ppd as ppd
+
+def TestOneInput(data):
+    try:
+        #fuzz.ppd is a fake file name
+        ppd.parse(data, "fuzz.ppd")
+    except Exception:
+        #parsing errors are expected
+        pass
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+if __name__ == "__main__":
+    main()
