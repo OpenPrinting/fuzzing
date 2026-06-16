3dada4f620a19156e50357105ccf8e74a077c739
Arthur Chan <arthur.chan@adalogics.com>
2026-06-16 20:50:07 +0100

    OSS-Fuzz: Fix fuzzers and build
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/cups-filters/oss_fuzz_build.sh
+++ projects/cups-filters/oss_fuzz_build.sh
@@ -1,60 +1,26 @@
 #!/bin/bash -eu
 
-# Set fPIE
-# export CFLAGS="$CFLAGS -fPIE"
-# export CXXFLAGS="$CFLAGS -fPIE"
-# export LDFLAGS="$CFLAGS -fPIE"
+export CC=${CC:-clang}
+export CXX=${CXX:-clang++}
+export CFLAGS="$CFLAGS -fPIE"
+export CXXFLAGS="$(echo "$CXXFLAGS" | sed 's/-stdlib=libc++//g') -fPIE"
 
-export CC=clang
-export CXX=clang++
-
-export CFLAGS="-fPIE"
-export CXXFLAGS="-fPIE"
-export LDFLAGS="-fPIE"
-
-export CFLAGS="$CFLAGS -fsanitize=$SANITIZER"
-export CXXFLAGS="$CXXFLAGS -fsanitize=$SANITIZER"
-export LDFLAGS="-fsanitize=$SANITIZER"
-
-# For regular sanitizers
-if [[ $SANITIZER == "coverage" ]]; then
-    export CFLAGS=""
-    export CXXFLAGS=""
-    export LDFLAGS=""
-elif [[ $SANITIZER == "undefined" ]]; then
-    export CFLAGS="$CFLAGS -fno-sanitize=function"
-    export CXXFLAGS="$CXXFLAGS -fno-sanitize=function"
-    export LDFLAGS="-fno-sanitize=function"
-fi
-
-# For fuzz introspector
-if [[ $SANITIZER == "introspector" ]]; then
-    export CFLAGS="-O0 -flto -fno-omit-frame-pointer -gline-tables-only -Wno-error=enum-constexpr-conversion -Wno-error=incompatible-function-pointer-types -Wno-error=int-conversion -Wno-error=deprecated-declarations -Wno-error=implicit-function-declaration -Wno-error=implicit-int -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -g"
-    export CXXFLAGS="-O0 -flto -fno-omit-frame-pointer -gline-tables-only -Wno-error=enum-constexpr-conversion -Wno-error=incompatible-function-pointer-types -Wno-error=int-conversion -Wno-error=deprecated-declarations -Wno-error=implicit-function-declaration -Wno-error=implicit-int -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -g"
-    export LDFLAGS="-flto"
-fi
+FUZZER_DIR=$SRC/fuzzing/projects/cups-filters
 
 # for libtool usage
 export PATH=$PATH:$SRC/cups-filters
-cp $SRC/fuzzing/projects/cups-filters/fuzzer/patch_qpdf_xobject $SRC/cups-filters/filter/pdftopdf/
+cp $FUZZER_DIR/fuzzer/patch_qpdf_xobject $SRC/cups-filters/filter/pdftopdf/
 
-# Prepare fuzz dir
-pushd $SRC/fuzzing/projects/cups-filters/
-# Show fuzzer version
-echo "OpenPrinting/fuzzing version: $(git rev-parse HEAD)"
-cp -r $SRC/fuzzing/projects/cups-filters/fuzzer/. $SRC/cups-filters/ossfuzz/
-popd
+echo "OpenPrinting/fuzzing version: $(git -C $FUZZER_DIR rev-parse HEAD)"
 
 # Build cups-filters
 pushd $SRC/cups-filters
-
-# Show build version
 echo "cups-filters version: $(git rev-parse HEAD)"
 
 # For multiple definition of `_cups_isalpha', `_cups_islower`, `_cups_toupper`
-export LDFLAGS="$LDFLAGS -Wl,--allow-multiple-definition" # rather important without this, the build will fail
+export LDFLAGS="${LDFLAGS:-} -Wl,--allow-multiple-definition"
 
-### Temperal fix bug due to libqpdf-dev 9
+### Temporal fix bug due to libqpdf-dev 9
 pushd $SRC/cups-filters/filter/pdftopdf/
 patch < patch_qpdf_xobject
 popd
@@ -62,16 +28,34 @@ popd
 ./autogen.sh
 ./configure --enable-static --disable-shared
 make # -j$(nproc)
-popd
 
-pushd $SRC/cups-filters/ossfuzz/
-# Build fuzzers
-make
-make ossfuzz
+# Build the PDF-parsing fuzzer (C++) against the qpdf-backed pdftopdf processor
+# objects produced above (minus the one that defines main()).
+QPDF_CFLAGS=$(pkg-config --cflags libqpdf 2>/dev/null || true)
+QPDF_LIBS=$(pkg-config --libs libqpdf 2>/dev/null || echo -lqpdf)
+CUPS_LDLIBS=$(cups-config --libs 2>/dev/null || echo -lcups)
+PDFTOPDF_OBJS=$(ls filter/pdftopdf/pdftopdf-*.o 2>/dev/null | grep -vE 'pdftopdf-pdftopdf\.o$')
+
+$CXX $CXXFLAGS -std=c++17 $QPDF_CFLAGS -I filter/pdftopdf -I filter -I . \
+    -c $FUZZER_DIR/fuzzer/fuzz_pdf.cc -o $WORK/fuzz_pdf.o
+$CXX $CXXFLAGS $WORK/fuzz_pdf.o $PDFTOPDF_OBJS \
+    .libs/libcupsfilters.a \
+    $LIB_FUZZING_ENGINE $QPDF_LIBS $CUPS_LDLIBS \
+    -Wl,--allow-multiple-definition \
+    -o $OUT/fuzz_pdf
+
+ldd $OUT/fuzz_pdf | awk '/=> \//{print $3}' | while read -r so; do
+    b=$(basename "$so")
+    case "$b" in
+        libc.so.*|libm.so.*|libpthread.so.*|libdl.so.*|librt.so.*|ld-linux*) continue ;;
+    esac
+    cp -L "$so" "$OUT/$b"
+done
+patchelf --force-rpath --set-rpath '$ORIGIN' $OUT/fuzz_pdf
 popd
 
 # Prepare corpus
-pushd $SRC/fuzzing/projects/cups-filters/seeds/
+pushd $FUZZER_DIR/seeds/
 for seed_folder in *; do
     zip -r $seed_folder.zip $seed_folder
 done
